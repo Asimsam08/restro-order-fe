22cbdfdc0a80c5604f017d52df31fe5daf4224d1
asim <asim.mahmood00786@gmail.com>
2025-07-01 20:18:48 +0530

    fixed socket connection url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,5 @@
 export const BASE_API_ENDPOINT = process.env.NEXT_PUBLIC_API_URL;
+export const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 export const menuItems = [
   { id: 1, name: "Margherita Pizza", price: 299 },
   { id: 2, name: "Paneer Tikka", price: 249 },
--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,13 +1,13 @@
 
 import { io } from "socket.io-client";
-import { BASE_API_ENDPOINT } from "@/utils/constant";
+import { SOCKET_URL } from "@/utils/constant";
 
 // const socket = io("http://localhost:8000", {
 //   withCredentials: true,
 //   transports: ["websocket"], 
 // });
 
-const socket = io(BASE_API_ENDPOINT, {
+const socket = io(SOCKET_URL, {
   withCredentials: true,
   transports: ["websocket"], 
 });
